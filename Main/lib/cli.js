f8afa64aa8b62efbf4828073785b1eceb00ed142
Cristofferb7 <136852733+Cristofferb7@users.noreply.github.com>
2023-07-26 17:27:07 -0400

    we added an else if statement to generate the svg content and to log it into the console

--- Main/lib/cli.js
+++ Main/lib/cli.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-const { Circle, Triangle, Square } = require(); //require file//
+const { log } = require('console');
+const { Circle, Triangle, Square } = require("./shapes"); //require file//
 class CLI {
   constructor() {
     this.title = '';
@@ -14,57 +15,55 @@ class CLI {
       .prompt([
         {
           type: 'input',
-          name: 'name',
-          message: 'Please enter your name',
+          name: 'text',
+          message: 'Please enter text',
+        },
+
+        {
+          type: 'input',
+          name: 'textColor',
+          message: 'Please enter text color',
+        },
+
+        {
+          type: 'list',
+          name: 'shape',
+          message: 'select shape',
+          choices: ['circle', 'triangle', 'square']
+        },
+
+        {
+          type: 'input',
+          name: 'shapeColor',
+          message: 'Please enter your shape color',
         },
       ])
-      .then(({ name }) => {
-        this.title = `${name}'s Tasks`;
-        return this.addTask();
+    
+      .then((answers) => {
+        console.log(answers);
+
+        if(answers.shape == "circle"){
+
+          const newObject = new Circle(answers.shapeColor, answers.textColor, answers.text);
+
+          const svgContent = newObject.generateSVG()
+
+          console.log(svgContent)
+
+        } else if(answers.shape == "triangle") {
+
+        } else if(answers.shape == "square") {
+          
+        }
+
+
       })
-      .then(() => {
-        // sort by priority so that priority tasks come before non-priority tasks
-        this.tasks.sort((a, b) =>
-          a.priority === b.priority ? 0 : a.priority && !b.priority ? -1 : 1
-        );
-        return writeFile(
-          join(__dirname, '..', 'output', 'tasks.html'),
-          createDocument(this.title, this.tasks)
-        );
-      })
-      .then(() => console.log('Created tasks.html'))
       .catch((err) => {
         console.log(err);
         console.log('Oops. Something went wrong.');
       });
   }
 
-  addTask() {
-    return inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'text',
-          message: 'Enter task',
-        },
-        {
-          type: 'confirm',
-          name: 'priority',
-          message: 'Is this a priority task?',
-        },
-        {
-          type: 'confirm',
-          name: 'confirmAddTask',
-          message: 'Would you like to add another task?',
-        },
-      ])
-      .then(({ text, priority, confirmAddTask }) => {
-        this.tasks.push({ text, priority });
-        if (confirmAddTask) {
-          return this.addTask();
-        }
-      });
-  }
 }
 
 module.exports = CLI;
